3df1bf43e0b268e3a56f16c3284e0b31773170d6
V3N0M <v3n0m@V3N0Ms-MacBook-Pro.local>
2017-09-18 22:51:18 -0700

    made changes to header and home component

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
 const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+import {DomSanitizer} from '@angular/platform-browser';
+import {MdIconRegistry} from '@angular/material';
+
 
 
 @Component({
@@ -9,20 +13,51 @@ const EMAIL_REGEX = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
+  firstName: string;
+  lastName: string;
+  tel: string;
+  address: string;
+  address2: string;
+  postCode: string;
+  favoriteSeason: string;
+  message: string;
+
+
+
+
+
+
+  sendValues(): void {
+  console.log(this.firstName + " " + this.lastName + " telphone " + this.tel + " address " + this.address + "address2 " + 
+this.address2 + " postal Code " + this.postCode + " " + this.favoriteSeason + " message " + this.message);
+  }
+
+
   emailFormControl = new FormControl('', [
     Validators.required,
     Validators.pattern(EMAIL_REGEX)]);
   
-  favoriteSeason: string;
+  
+  
+  postalCode: string;
   
     seasons = [
       'Alarms',
       'Access Control',
       'Security Cameras',
+      'Point of Sales(POS)'
       
     ];  
+    
   
-  constructor() { }
+  constructor(private router:Router, iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) { iconRegistry.addSvgIcon(
+    'thumbs-up',
+    sanitizer.bypassSecurityTrustResourceUrl('assets/credit-card.svg'));
+  }
+
+  function(chosenCity: string) {
+    console.log(chosenCity);
+  }
 
   ngOnInit() {
     var camera1 = "/assets/camera1.jpg";
